123c16777e097f572613ccc3e9f9a55e91d795af
GISPHN <spi4949@gmail.com>
2026-08-19 08:47:24 +0900

    Enable v1.1.2 test fixes

--- gcham_datapack_builder/__init__.py
+++ gcham_datapack_builder/__init__.py
@@ -1,6 +1,11 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2026 Ryo Horiike / GISPHN
 
+
 def classFactory(iface):
+    from .v112_patches import apply_patches
+
+    apply_patches()
     from .plugin import GCHAMDataPackBuilderPlugin
+
     return GCHAMDataPackBuilderPlugin(iface)
